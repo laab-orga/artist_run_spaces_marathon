fdf5165687b0680eaff46229229051d34b6b9bc2
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-08 11:52:46 +0200

    Update script.js

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function(){
+  
+  $('#clust').change(function(){
+    console.log($("#clust option:selected").val());
+    var fichier = $("#clust option:selected").val();
+    draw_graph(fichier);
+  });
+});
