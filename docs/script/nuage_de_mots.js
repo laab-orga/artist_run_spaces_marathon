fbd0289cdddbd8cebb0d18a994607a9405c64d01
GaelleNovales <47526451+GaelleNovales@users.noreply.github.com>
2020-04-08 17:13:57 +0200

    fichier js pour le nuage de mots

---
+++ docs/script/nuage_de_mots.js
@@ -0,0 +1,32 @@
+var requestURL = 'data/comptage/graphe1.json';
+	var request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+		var data = request.response;
+		console.log(data);
+	
+	
+	
+	
+Highcharts.chart('container', {
+    accessibility: {
+        screenReaderSection: {
+            beforeChartFormat: '<h5>{chartTitle}</h5>' +
+                '<div>{chartSubtitle}</div>' +
+                '<div>{chartLongdesc}</div>' +
+                '<div>{viewTableButton}</div>'
+        }
+    },
+    series: [{
+        type: 'wordcloud',
+        data: data["cluster1"],
+        name: 'Occurrences'
+    }],
+    title: {
+        text: 'Nuage des mots clés du cluster'
+    }
+});
+
+	}
