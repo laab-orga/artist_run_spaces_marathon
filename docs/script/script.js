fb7c3a4946089dc5c0f4720e67b41fb36d297000
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 10:31:00 +0200

    Update script.js

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,13 +1,14 @@
 
 $(document).ready(function(){
-  
+  console.log('script');
+  var fichier = $("#clust option:selected").val();
+  draw_graph(fichier);
     $('#clust').change(function(){
       console.log($("#clust option:selected").val());
-      var fichier = $("#clust option:selected").val();
+      fichier = $("#clust option:selected").val();
       draw_graph(fichier);
       wordcloud(fichier);
-	  adjacence(fichier);
-	  Parametre(fichier);
+	    adjacence(fichier);
       $('#content_lda').html(`<object class="w-100 lda" data="data/LDA/cluster`+fichier+`lda.html" type="text/html" > <!-- Mettre chemin vers fichier LDA-->`);
     });
   });
