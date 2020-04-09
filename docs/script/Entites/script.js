a7d78b4823331ca79016735b7c4b3bf0836ac5c3
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 11:38:56 +0200

    Update script.js

--- docs/script/Entites/script.js
+++ docs/script/Entites/script.js
@@ -1,7 +1,8 @@
 console.log('script');
 $(document).ready(function(){
-    draw_graph('graph_scrap',1);
+    draw_graph('TOTALY_scrap',1);
     draw_graph('Word_for_graph_word_scrap',2);
     wordcloud('wordcountscrap',1);
     wordcloud('wordcountscrap',2);
+    adjacence('HEATMAP_SCRAP_PROJET');
 });
