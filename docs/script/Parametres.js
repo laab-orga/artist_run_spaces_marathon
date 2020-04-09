31b5f7657f6bd6f6f53f8f22bd5bf7fdd43d5a07
GaelleNovales <47526451+GaelleNovales@users.noreply.github.com>
2020-04-09 16:37:47 +0200

    mise a jour du tableau html renvoyé par le js

--- docs/script/Parametres.js
+++ docs/script/Parametres.js
@@ -28,7 +28,47 @@ function Parametre(fichier){
 			//tableauHTML.hidden = false;
 			// TABLEAU DEGRÉ 
 
-			tableau += "<titre> Tableau des paramètres du "+cluster+"</titre><table><tr><TH>Paramètre</TH><TH>Valeur</TH><TH>Name</TH></tr><tr><TH> Taille du Graphe </TH><td>"+TailleGraph+"</td><td></td></tr><tr><TH> Degré moyen </TH><td>"+degréMoyen+"</td><td></td></tr><tr><th>Degré max</th><td>"+degréMax+"</td><td>"+degréMaxName+"</td></tr><tr><th>Degré min</th><td>"+degréMin+"</td><td>"+degréMinName+"</td></tr><tr><th>Coefficient de Clustering Moyen</th><td>"+coefClusterMoyen+"</td><td></td></tr><tr><TH> Connectivité moyen </TH><td>"+connectivitéMoyenneGraph+"</td><td></td></tr></table>";
+			/*tableau += "<titre> Tableau des paramètres du "+cluster+"</titre>";*/
+			tableau += "<table id='tableSarah'>";
+			tableau += "<tr>";
+				tableau += "<th>Paramètre</th>";
+				tableau += "<th>Valeur</th>";
+				tableau +="<th>Variable</th>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td> Taille du Graphe </td>";
+				tableau +="<td>"+TailleGraph+"</td>";
+				tableau +="<td></td>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td> Degré moyen </td>";
+				tableau +="<td>"+Math.round(degréMoyen*1000)/1000+"</td>";//arrondi au troisieme chiffre apres la virgule
+				/*tableau +="<td>"+degréMoyen+"</td>";*/
+				tableau +="<td></td>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td>Degré max</td>";
+				tableau +="<td>"+degréMax+"</td>";
+				tableau +="<td>"+degréMaxName+"</td>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td>Degré min</td>";
+				tableau +="<td>"+degréMin+"</td>";
+				tableau +="<td>"+degréMinName+"</td>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td>Coefficient de Clustering Moyen</td>";
+				tableau +="<td>"+Math.round(coefClusterMoyen*1000)/1000+"</td>";
+				/*tableau +="<td>"+coefClusterMoyen+"</td>";*/
+				tableau +="<td></td>";
+			tableau +="</tr>";
+			tableau +="<tr>";
+				tableau +="<td> Connectivité moyen </td>";
+				tableau +="<td>"+Math.round(connectivitéMoyenneGraph*1000)/1000+"</td>";
+				/*tableau +="<td>"+connectivitéMoyenneGraph+"</td>";*/
+				tableau +="<td></td>";
+			tableau +="</tr>";
+			tableau +="</table>";
 
 
 // Ajustement HTML
