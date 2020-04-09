2140affcb1bdba77ea6313987572a6c2b490eb85
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 15:43:13 +0200

    Update script.js

--- docs/script/Concept/script.js
+++ docs/script/Concept/script.js
@@ -1,8 +1,6 @@
 console.log('script');
 $(document).ready(function(){
-    draw_graph('graph_scrap',1);
-    draw_graph('Word_scrap2',2);
-    wordcloud('wordcountscrap',1);
-    //wordcloud('wordcountscrap',2);
-    adjacence('HEATMAP_SCRAP_PROJET');
+    draw_graph('graph_word_concept_final',1);
+    draw_graph('graph_concept_projet',2);
+    wordcloud('concept_wordcloud',1);
 });
