6c42bcd47792b641007c6d3b9ee9e071a64aba2d
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 17:13:41 +0200

    Add files via upload

--- docs/script/Entites/script.js
+++ docs/script/Entites/script.js
@@ -1,7 +1,10 @@
+console.log('script');
 $(document).ready(function(){
     draw_graph('graph_scrap',1);
     draw_graph('Word_scrap2',2);
     wordcloud('wordcountscrap',1);
     //wordcloud('wordcountscrap',2);
     adjacence('HEATMAP_SCRAP_PROJET');
+    parametre_Scrap_projet();
+    parametre_Scrap_words();
 });
