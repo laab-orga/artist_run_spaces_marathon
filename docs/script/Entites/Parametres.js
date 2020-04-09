42a2af21cd90e53023d2e49e84f27e997bf55dff
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 17:13:14 +0200

    Add files via upload

--- docs/script/Entites/Parametres.js
+++ docs/script/Entites/Parametres.js
@@ -1,6 +1,7 @@
-console.log('Parametres');
-function Parametre(fichier){ 
-	var requestURL = 'data/INFO_JSONV4.json';
+//////////////////////////////////////////////////////////////Page 2////////////////////////////////////////////////
+
+function parametre_Scrap_projet(){
+	var requestURL = 'scrap_web_link/INFOGRAPHE/info_scrap_project.json'; // a changer 
 	var request = new XMLHttpRequest();
 	request.open('GET', requestURL);
 	request.responseType = 'json';
@@ -8,32 +9,53 @@ function Parametre(fichier){
 	request.onload = function() {
 		var data = request.response;
 	// définition des variables 
-		var cluster = 'cluster'+(fichier); 
-		var degréMoyen=data[cluster][0]["degree_moyen"];
-		var degréMax=data[cluster][0]['degree_max'];
-		var degréMaxName=data[cluster][0]['noeud_max_degree'];
-		var degréMin=data[cluster][0]['degree_min'];
-		var degréMinName=data[cluster][0]['noeud_min_degree'];
-		var coefClusterMoyen=data[cluster][0]['coefficient_clustering'];
-		var TailleGraph=data[cluster][0]['size'];
-		var connectivitéMoyenneGraph =data[cluster][0]['moy_connect_noeud'];
-	
-
+		var degréMoyen=data["degree_moyen"];
+		var degréMax=data['degree_max'];
+		var degréMaxName=data['noeud_max_degree'];
+		var degréMin=data['degree_min'];
+		var degréMinName=data['noeud_min_degree'];
+		var coefClusterMoyen=data['coefficient_clustering'];
+		var TailleGraph=data['size'];
 		
 // Création du tableau 
-			var tableaudegre = "";
-			
-			titre = 'Paramètres du ' ;
-			titre+=cluster;
-			//tableauHTML = document.getElementById("tableau_ULIS");
-			//tableauHTML.hidden = false;
-			// TABLEAU DEGRÉ 
-
-			tableaudegre += "<titre> Tableau des paramètres du "+cluster+"</titre><table><tr><TH>Paramètre</TH><TH>Valeur</TH><TH>Name</TH></tr><tr><TH> Taille du Graphe </TH><td>"+TailleGraph+"</td><td></td></tr><tr><TH> Degré moyen </TH><td>"+degréMoyen+"</td><td></td></tr><tr><th>Degré max</th><td>"+degréMax+"</td><td>"+degréMaxName+"</td></tr><tr><th>Degré min</th><td>"+degréMin+"</td><td>"+degréMinName+"</td></tr><tr><th>Coefficient de Clustering Moyen</th><td>"+coefClusterMoyen+"</td><td></td></tr><tr><TH> Connectivité moyen </TH><td>"+connectivitéMoyenneGraph+"</td><td></td></tr></table>";
+			var tableau = "";
+			tableau += "<titre> Tableau des paramètres Scrap Projet </titre><table><tr><TH>Paramètre</TH><TH>Valeur</TH><TH>Name</TH></tr><tr><TH> Taille du Graphe </TH><td>"+TailleGraph+"</td><td></td></tr><tr><TH> Degré moyen </TH><td>"+degréMoyen+"</td><td></td></tr><tr><th>Degré max</th><td>"+degréMax+"</td><td>"+degréMaxName+"</td></tr><tr><th>Degré min</th><td>"+degréMin+"</td><td>"+degréMinName+"</td></tr><tr><th>Coefficient de Clustering Moyen</th><td>"+coefClusterMoyen+"</td><td></td></tr></table>";
 
 
 // Ajustement HTML
-		var ex = document.getElementById("param");
-		ex.innerHTML = tableaudegre;
-		}
-	}	
+		var ex = document.getElementById("param_scrap_pro");
+		ex.innerHTML = tableau;
+	}
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////// POUR LES SCRAP WORDS
+function parametre_Scrap_words(){
+	var requestURL = 'scrap_web_link/INFOGRAPHE/INFO_FOR_WORD_SCRAP.json'; //a changer
+	var request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+		var data = request.response;
+	// définition des variables 
+		var degréMoyen=data["degree_moyen"];
+		var degréMax=data['degree_max'];
+		var degréMaxName=data['noeud_max_degree'];
+		var degréMin=data['degree_min'];
+		var degréMinName=data['noeud_min_degree'];
+		var coefClusterMoyen=data['coefficient_clustering'];
+		var TailleGraph=data['size'];
+
+// Création du tableau 
+			var tableau = "";
+
+
+			tableau += "<titre> Tableau des paramètres Scrap des Mots</titre><table><tr><TH>Paramètre</TH><TH>Valeur</TH><TH>Name</TH></tr><tr><TH> Taille du Graphe </TH><td>"+TailleGraph+"</td><td></td></tr><tr><TH> Degré moyen </TH><td>"+degréMoyen+"</td><td></td></tr><tr><th>Degré max</th><td>"+degréMax+"</td><td>"+degréMaxName+"</td></tr><tr><th>Degré min</th><td>"+degréMin+"</td><td>"+degréMinName+"</td></tr><tr><th>Coefficient de Clustering Moyen</th><td>"+coefClusterMoyen+"</td><td></td></tr></table>";
+
+		
+		var ex = document.getElementById('param_scrap_words');
+		ex.innerHTML= tableau;
+	}
+}
