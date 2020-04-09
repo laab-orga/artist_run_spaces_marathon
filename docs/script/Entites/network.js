502613aa81bfbdb22a23baa6290ce592e6d57f12
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 18:59:41 +0200

    Update network.js

--- docs/script/Entites/network.js
+++ docs/script/Entites/network.js
@@ -1,7 +1,7 @@
 var width = 800;
 var height = 600;
 
-var color2 = d3.scaleOrdinal(d3.schemeGreys[9]);
+var color2 = ['#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252'];
 function draw_graph(fichier,div){
   console.log(color2);
   $('#viz_'+div).html('');
@@ -69,7 +69,7 @@ function draw_graph(fichier,div){
       .append("circle")
       .attr("stroke-width", "2px")
       .attr("stroke","black")
-      .attr("fill", function(d) { return color2(Math.min(7,d.degree)); }) 
+      .attr("fill", function(d) { return color2[Math.min(7,d.degree)]; }) 
       .attr("r", function(d) { return 5+ 10*d.clustering_coeff; } )
       
   node.on("mouseover", focus).on("mouseout", unfocus);
