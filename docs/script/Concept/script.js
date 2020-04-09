fe2bdb87356003022c46c84b26b5d26d307b6260
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 16:50:02 +0200

    Update script.js

--- docs/script/Concept/script.js
+++ docs/script/Concept/script.js
@@ -1,6 +1,6 @@
 console.log('script');
 $(document).ready(function(){
-    draw_graph('graph_word_concept_final',1);
-    draw_graph('graph_concept_projet',2);
+    draw_graph('graph_concept_projet',1);
+    draw_graph('graph_word_concept_final',2);
     wordcloud('concept_wordcloud',1);
 });
