c48767c53fbe0a98b4557c2722b48a68d6f08bbb
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 15:22:25 +0200

    Update script.js

--- docs/script/script.js
+++ docs/script/script.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
   var fichier = $("#clust option:selected").val();
   global_graph(fichier);
   draw_graph(fichier);
-      console.log(fichier);
     $('#clust').change(function(){
       console.log(fichier);
-      console.log($("#clust option:selected").val());
       fichier = $("#clust option:selected").val();
       draw_graph(fichier);
       wordcloud(fichier);
@@ -15,9 +13,11 @@ $(document).ready(function(){
       global_graph(fichier);
       if(fichier!='Global'){
         $('.container3').hide();
+        console.log('hide');
       }
       else{
         $('.container3').show();
+        console.log('show');
       }
       $('#content_lda').html(`<object class="w-100 lda" data="data/LDA/cluster`+fichier+`lda.html" type="text/html" > <!-- Mettre chemin vers fichier LDA-->`);
     });
