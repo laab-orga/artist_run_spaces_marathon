f02f3f87d6d3a1c34cc41448c40e1b9968c01b25
acanetti <47980384+acanetti@users.noreply.github.com>
2020-04-09 18:43:13 +0200

    Add files via upload

--- docs/script/Concept/script.js
+++ docs/script/Concept/script.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
     draw_graph('graph_concept_projet',1);
     draw_graph('graph_word_concept_final',2);
     wordcloud('concept_wordcloud',1);
+    parametre_concept_pro();
+    parametre_concept_words();
 });
